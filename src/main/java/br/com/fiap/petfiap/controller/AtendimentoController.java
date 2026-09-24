912c6881daebb66b00e13789c1600c14a3256e4f
Joao Victor <joaovictoralvesabreu0812@gmail.com>
2026-09-24 20:43:26 -0300

    refactor: clean02 - remove codigo morto de desconto

--- src/main/java/br/com/fiap/petfiap/controller/AtendimentoController.java
+++ src/main/java/br/com/fiap/petfiap/controller/AtendimentoController.java
@@ -103,11 +103,4 @@ public class AtendimentoController {
         }
     }
 
-    // -----------------------------------------------------------------
-    // Fidelidade (futuro) - implementar quando o time aprovar:
-    // - desconto de 10% para tutores com mais de 500 pontos
-    // - dobro de pontos em novembro amarelo (castracao)
-    private double calcularDescontoFidelidade(int pontos) {
-        return pontos * 0.1;
-    }
 }
