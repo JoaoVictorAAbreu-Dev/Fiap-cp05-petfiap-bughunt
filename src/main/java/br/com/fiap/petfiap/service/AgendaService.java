edf9088e9321cb2d0f9ad9f62c18ccf434b6a4c1
Joao Victor <joaovictoralvesabreu0812@gmail.com>
2026-09-24 20:43:41 -0300

    refactor: clean04 - remove recibo em console do service

--- src/main/java/br/com/fiap/petfiap/service/AgendaService.java
+++ src/main/java/br/com/fiap/petfiap/service/AgendaService.java
@@ -31,8 +31,6 @@ public class AgendaService {
             }
         }
         Atendimento salvo = repository.save(novo);
-        System.out.println("Recibo: atendimento " + salvo.getProtocolo()
-                + " agendado para " + salvo.getPetNome() + " (tutor " + salvo.getTutorNome() + ")");
         return salvo;
     }
 
